bd299c37048b77ab7ff852236be1ddbeb3e4c1d3
echo1097 <34988940+echo1097@users.noreply.github.com>
2025-01-18 19:32:49 -0800

    2.0

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
+
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,20 +9,18 @@ module.exports = {
 
     const start = Date.now();
 
-
     const embed = new EmbedBuilder()
       .setTitle('ping')
       .setColor('#5865F2');
 
-
-    await interaction.reply({ embeds: [embed], ephemeral: true });
-
+    await interaction.reply({ 
+      embeds: [embed], 
+      flags: MessageFlags.Ephemeral 
+    });
 
     const responseTime = Date.now() - start;
 
-
     embed.setDescription(`${responseTime}ms`);
-
 
     await interaction.editReply({ embeds: [embed] });
   },
