4472af02796a10b07e1b1d6f14ca05b1f8ade99f
echo1097 <34988940+echo1097@users.noreply.github.com>
2025-01-18 19:31:36 -0800

    2.0

--- commands/utility/addping.js
+++ commands/utility/addping.js
@@ -1,26 +1,35 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('optin')
-        .setDescription('start svs pings'),
+        .setDescription('start SVS pings'),
     async execute(interaction) {
         const role = interaction.guild.roles.cache.get('roleid');
-        
+
+        if (!role) {
+            const embed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setDescription('role doesnt exist/improper roldid passed check the files (addping.js)');
+
+            await interaction.reply({ embeds: [embed], flags: [MessageFlags.Ephemeral] });
+            return;
+        }
+
         if (interaction.member.roles.cache.has('roleid')) {
             const embed = new EmbedBuilder()
                 .setColor(0x00FF00)
                 .setDescription('you already have the role');
-                
-            await interaction.reply({ embeds: [embed], ephemeral: true });
+
+            await interaction.reply({ embeds: [embed], flags: [MessageFlags.Ephemeral] });
             return;
         }
-        
+
         await interaction.member.roles.add(role);
         const embed = new EmbedBuilder()
             .setColor(0x00FF00)
             .setDescription('added role - `/optout` to remove');
-            
-        await interaction.reply({ embeds: [embed], ephemeral: true });
+
+        await interaction.reply({ embeds: [embed], flags: [MessageFlags.Ephemeral] });
     },
 };
